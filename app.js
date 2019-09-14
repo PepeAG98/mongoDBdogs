78c54b7d989f5ac734a9e4e2d49656e456c6288a
Pepe Alvarado <A01273109@itesm.mx>
2019-09-13 22:06:40 -0500

    endpoints tarea agregados

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const dogsRouter = require('./routes/dogs');
 
-var app = express();
+mongoose.connect('mongodb://127.0.0.1:27017/pets2DB',
+    { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -15,6 +20,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/dogs', dogsRouter);
+
+app.get('*', (req,res) => {
+    res.status(404).send('Route not found');
+});
 
 module.exports = app;
